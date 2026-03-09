30988d4ee9139274d7fc8a667bcb7db7fca2f2f2
ViniciusPiresB <viniciuspbarreto1@gmail.com>
2026-03-09 11:07:14 -0300

    Inicio calculadora

---
+++ src/main.py
@@ -0,0 +1,26 @@
+def calculadora():
+    print("=== Calculadora em Python ===")
+
+    while True:
+        try:
+            num1 = float(input("Digite o primeiro número: "))
+            operador = input("Digite o operador (+, -, *, /): ")
+            num2 = float(input("Digite o segundo número: "))
+
+            if operador == "+":
+                resultado = num1 + num2
+            elif operador == "-":
+                resultado = num1 - num2
+
+            print(f"Resultado: {resultado}")
+
+        except ValueError:
+            print("Entrada inválida! Digite números.")
+
+        continuar = input("Deseja fazer outra conta? (s/n): ").lower()
+        if continuar != "s":
+            print("Calculadora encerrada.")
+            break
+
+
+calculadora()
